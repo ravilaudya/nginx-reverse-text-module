fa6012679e25d0e85748bc9ffaab57169132d0aa
Ravi <ravi@o2.local>
2018-07-27 21:44:01 +0530

    send chunked response

--- ngx_http_reverse_text_module.c
+++ ngx_http_reverse_text_module.c
@@ -14,8 +14,10 @@
 static char *ngx_http_reverse_text(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 static ngx_int_t ngx_http_reverse_text_handler(ngx_http_request_t *r);
 
+#define DEFAULT_BUFFER_SIZE     (4 * 1024)
+
 /**
- * This module provided directive: hello world.
+ * This module provided directive: reverse_text.
  *
  */
 static ngx_command_t ngx_http_reverse_text_commands[] = {
@@ -66,12 +68,14 @@ ngx_module_t ngx_http_reverse_text_module = {
  * Send content length, content type and status headers
  */
 static ngx_int_t reverse_text_send_headers(ngx_http_request_t *r) {
-    ngx_int_t    rc, len;
+    ngx_int_t    rc, len, buffers_count;
     ngx_chain_t  *in;
 
     len = 0;
+    buffers_count = 0;
 
     for (in = r->request_body->bufs; in; in = in->next) {
+        buffers_count++;
         len += ngx_buf_size(in->buf);
     }
     r->headers_out.content_type.len = sizeof("text/plain") - 1;
@@ -79,7 +83,7 @@ static ngx_int_t reverse_text_send_headers(ngx_http_request_t *r) {
     r->headers_out.status = NGX_HTTP_OK;
     r->headers_out.content_length_n = len;
 
-    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Sent content length header with value: %d", len);
+    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Sent content length header with value: %d, total buffers: %d", len, buffers_count);
     rc = ngx_http_send_header(r);
 
     return rc;
@@ -88,9 +92,9 @@ static ngx_int_t reverse_text_send_headers(ngx_http_request_t *r) {
 /**
  * Inplace string reverse in buffer
  */
-static void in_place_string_reverse(ngx_http_request_t *r, ngx_buf_t *buffer) {
+static void in_place_buffer_reverse(ngx_http_request_t *r, ngx_buf_t *buffer) {
     unsigned char *lhs = buffer->start, *rhs = buffer->last;
-    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "About to reverse string: %s", lhs);
+//    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "About to reverse string: %s", lhs);
     if (!lhs || !rhs || !*lhs || !*(lhs+1)) {
         return;
     }
@@ -100,7 +104,74 @@ static void in_place_string_reverse(ngx_http_request_t *r, ngx_buf_t *buffer) {
         *lhs++ = *rhs;
         *rhs = tmp;
     }
-    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "After reverse: %s", lhs);
+//    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "After reverse: %s", buffer->start);
+}
+
+static ngx_int_t max(ngx_int_t a, ngx_int_t b) {
+    return (a > b)? a: b;
+}
+static ngx_int_t min(ngx_int_t a, ngx_int_t b) {
+    return (a < b)? a: b;
+}
+
+static ngx_int_t send_file_chunked_response(ngx_http_request_t *r, ngx_buf_t *buf) {
+    ngx_int_t     rc, buf_size, next_chunk_size, remaining_size, offset;
+    u_char        *in_memory_buf;
+    ngx_buf_t     *buffer_to_send;
+    ngx_chain_t   out;
+
+    buf_size = ngx_buf_size(buf);
+    next_chunk_size = min(buf_size, DEFAULT_BUFFER_SIZE);
+    remaining_size = buf_size - next_chunk_size;
+    offset = max(buf_size - next_chunk_size, 0);
+    rc = NGX_OK;
+
+    in_memory_buf = ngx_palloc(r->connection->pool, next_chunk_size);
+    if (in_memory_buf == NULL) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Can not allocate memory for buffer to read from file: %s, size: %d", buf->file->name.data, buf_size);
+        return NGX_ERROR;
+    }
+    buffer_to_send = ngx_palloc(r->connection->pool, sizeof(ngx_buf_t *));
+    if (buffer_to_send == NULL) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Can not allocate memory for buffer to send to client, for data from file: %s, size: %d", buf->file->name.data, next_chunk_size);
+        return NGX_ERROR;
+    }
+
+    while (next_chunk_size > 0) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Data is in file: %s, size: %d, offset: %d", buf->file->name.data, next_chunk_size, offset);
+        rc = ngx_read_file(buf->file, in_memory_buf, next_chunk_size, offset);
+        if (rc < NGX_OK) {
+            ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Error reading from file: %s, size: %d, error: %d", buf->file->name.data, next_chunk_size, rc);
+            return NGX_ERROR;
+        }
+        buffer_to_send->start = buffer_to_send->pos = in_memory_buf;
+        buffer_to_send->end = buffer_to_send->last =  in_memory_buf + next_chunk_size;
+        in_place_buffer_reverse(r, buffer_to_send);
+
+        buffer_to_send->last_buf = (r == r->main) ? 1: 0;
+        buffer_to_send->last_in_chain = 1;
+        buffer_to_send->memory = 1;
+
+        out.buf = buffer_to_send;
+        out.next = NULL;
+
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Sending file buffer with size: %d", next_chunk_size);
+
+        rc = ngx_http_output_filter(r, &out);
+        if (rc != NGX_OK) {
+            ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Error sending data to output filter from file: %s, size: %d, error: %d", buf->file->name.data, next_chunk_size, rc);
+            return rc;
+        }
+
+        next_chunk_size = min(remaining_size, DEFAULT_BUFFER_SIZE);
+        remaining_size = remaining_size - next_chunk_size;
+        offset = max(offset - next_chunk_size, 0);
+    }
+    return rc;
+}
+
+static ngx_int_t send_file_buffer(ngx_http_request_t *r, ngx_buf_t *buf) {
+    return send_file_chunked_response(r, buf);
 }
 
 /**
@@ -110,14 +181,14 @@ static ngx_int_t send_current_buffer(ngx_http_request_t *r, ngx_buf_t  *buf) {
     ngx_chain_t  out;
 
     if (buf->in_file == 1) {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Data is in file.. will send later... %s", buf->file->name.data);
-        return NGX_OK;
+        return send_file_buffer(r, buf);
     }
-    in_place_string_reverse(r, buf);
+    in_place_buffer_reverse(r, buf);
     buf->last_buf = (r == r->main) ? 1: 0;
     buf->last_in_chain = 1;
     out.buf = buf;
     out.next = NULL;
+    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Sending buffer with size: %d", ngx_buf_size(buf));
 
     return ngx_http_output_filter(r, &out);
 }
