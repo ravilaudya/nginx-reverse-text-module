6b0412bfa485ffe7c0782365f4f8241940e4f8de
Ravi <ravi@o2.local>
2018-07-28 13:31:24 +0530

    refactor: writing to client from file

--- ngx_http_reverse_text_module.c
+++ ngx_http_reverse_text_module.c
@@ -16,6 +16,12 @@ static ngx_int_t ngx_http_reverse_text_handler(ngx_http_request_t *r);
 
 #define DEFAULT_BUFFER_SIZE     (4 * 1024)
 
+typedef struct {
+    ngx_int_t next_chunk_size;
+    ngx_int_t remaining_size;
+    ngx_int_t offset;
+} ngx_file_chunk_t;
+
 /**
  * This module provided directive: reverse_text.
  *
@@ -114,58 +120,75 @@ static ngx_int_t min(ngx_int_t a, ngx_int_t b) {
     return (a < b)? a: b;
 }
 
-static ngx_int_t send_file_chunked_response(ngx_http_request_t *r, ngx_buf_t *buf) {
-    ngx_int_t     rc, buf_size, next_chunk_size, remaining_size, offset;
-    u_char        *in_memory_buf;
-    ngx_buf_t     *buffer_to_send;
+static ngx_file_chunk_t find_next_file_chunk(ngx_buf_t *buf, ngx_int_t remaining_size, ngx_int_t offset) {
+    ngx_file_chunk_t  file_chunk;
+
+    file_chunk.next_chunk_size = min(remaining_size, DEFAULT_BUFFER_SIZE);
+    file_chunk.remaining_size = remaining_size - file_chunk.next_chunk_size;
+    file_chunk.offset = max(offset - file_chunk.next_chunk_size, 0);
+
+    return file_chunk;
+}
+
+static void build_buffer_to_send(ngx_http_request_t *r, ngx_buf_t *buffer_to_send, u_char *in_memory_buf, ngx_int_t next_chunk_size) {
+    buffer_to_send->start = buffer_to_send->pos = in_memory_buf;
+    buffer_to_send->end = buffer_to_send->last =  in_memory_buf + next_chunk_size;
+    in_place_buffer_reverse(r, buffer_to_send);
+
+    buffer_to_send->last_buf = (r == r->main) ? 1: 0;
+    buffer_to_send->last_in_chain = 1;
+    buffer_to_send->memory = 1;
+}
+
+static ngx_int_t build_buffer_and_send_to_client(ngx_http_request_t *r, ngx_buf_t *buffer_to_send, u_char *in_memory_buf, ngx_int_t next_chunk_size) {
     ngx_chain_t   out;
 
+    build_buffer_to_send(r, buffer_to_send, in_memory_buf, next_chunk_size);
+    out.buf = buffer_to_send;
+    out.next = NULL;
+
+    ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Sending file buffer with size: %d", next_chunk_size);
+
+    return ngx_http_output_filter(r, &out);
+}
+
+static ngx_int_t send_file_chunked_response(ngx_http_request_t *r, ngx_buf_t *buf) {
+    ngx_int_t     rc, buf_size;
+    u_char        *in_memory_buf;
+    ngx_buf_t     *buffer_to_send;
+    ngx_file_chunk_t  file_chunk;
+
     buf_size = ngx_buf_size(buf);
-    next_chunk_size = min(buf_size, DEFAULT_BUFFER_SIZE);
-    remaining_size = buf_size - next_chunk_size;
-    offset = max(buf_size - next_chunk_size, 0);
+    file_chunk = find_next_file_chunk(buf, buf_size, buf_size);
+
     rc = NGX_OK;
 
-    in_memory_buf = ngx_palloc(r->connection->pool, next_chunk_size);
+    in_memory_buf = ngx_palloc(r->connection->pool, file_chunk.next_chunk_size);
     if (in_memory_buf == NULL) {
         ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Can not allocate memory for buffer to read from file: %s, size: %d", buf->file->name.data, buf_size);
         return NGX_ERROR;
     }
     buffer_to_send = ngx_palloc(r->connection->pool, sizeof(ngx_buf_t *));
     if (buffer_to_send == NULL) {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Can not allocate memory for buffer to send to client, for data from file: %s, size: %d", buf->file->name.data, next_chunk_size);
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Can not allocate memory for buffer to send to client, for data from file: %s, size: %d", buf->file->name.data, file_chunk.next_chunk_size);
         return NGX_ERROR;
     }
 
-    while (next_chunk_size > 0) {
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Data is in file: %s, size: %d, offset: %d", buf->file->name.data, next_chunk_size, offset);
-        rc = ngx_read_file(buf->file, in_memory_buf, next_chunk_size, offset);
+    while (file_chunk.next_chunk_size > 0) {
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Data is in file: %s, size: %d, offset: %d", buf->file->name.data, file_chunk.next_chunk_size, file_chunk.offset);
+        rc = ngx_read_file(buf->file, in_memory_buf, file_chunk.next_chunk_size, file_chunk.offset);
         if (rc < NGX_OK) {
-            ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Error reading from file: %s, size: %d, error: %d", buf->file->name.data, next_chunk_size, rc);
+            ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Error reading from file: %s, size: %d, error: %d", buf->file->name.data, file_chunk.next_chunk_size, rc);
             return NGX_ERROR;
         }
-        buffer_to_send->start = buffer_to_send->pos = in_memory_buf;
-        buffer_to_send->end = buffer_to_send->last =  in_memory_buf + next_chunk_size;
-        in_place_buffer_reverse(r, buffer_to_send);
+        rc = build_buffer_and_send_to_client(r, buffer_to_send, in_memory_buf, file_chunk.next_chunk_size);
 
-        buffer_to_send->last_buf = (r == r->main) ? 1: 0;
-        buffer_to_send->last_in_chain = 1;
-        buffer_to_send->memory = 1;
-
-        out.buf = buffer_to_send;
-        out.next = NULL;
-
-        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Sending file buffer with size: %d", next_chunk_size);
-
-        rc = ngx_http_output_filter(r, &out);
         if (rc != NGX_OK) {
-            ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Error sending data to output filter from file: %s, size: %d, error: %d", buf->file->name.data, next_chunk_size, rc);
+            ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "Error sending data to output filter from file: %s, size: %d, error: %d", buf->file->name.data, file_chunk.next_chunk_size, rc);
             return rc;
         }
 
-        next_chunk_size = min(remaining_size, DEFAULT_BUFFER_SIZE);
-        remaining_size = remaining_size - next_chunk_size;
-        offset = max(offset - next_chunk_size, 0);
+        file_chunk = find_next_file_chunk(buf, file_chunk.remaining_size, file_chunk.offset);
     }
     return rc;
 }
